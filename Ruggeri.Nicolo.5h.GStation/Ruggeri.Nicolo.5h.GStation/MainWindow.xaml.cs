40e0c69c74eabdbd36a13a2fda0d48ecfc9e6003
Nicolò Ruggeri <nruggeri98@gmail.com>
2017-05-20 12:27:50 +0200

    Add files via upload

---
+++ Ruggeri.Nicolo.5h.GStation/Ruggeri.Nicolo.5h.GStation/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace Ruggeri.Nicolo._5h.GStation
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVisualizzaCalendar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                UserCredential credential;
+
+                using (var stream =
+                    new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = System.Environment.GetFolderPath(
+                        System.Environment.SpecialFolder.Personal);
+                    credPath = System.IO.Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                    Console.WriteLine("Credential file saved to: " + credPath);
+                }
+
+                // Create Google Calendar API service.
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                // Define parameters of request.
+                EventsResource.ListRequest request = service.Events.List("primary");
+                request.TimeMin = DateTime.Now;
+                request.ShowDeleted = false;
+                request.SingleEvents = true;
+                request.MaxResults = 10;
+                request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+                // List events.
+                Events events = request.Execute();
+                Console.WriteLine("Upcoming events:");
+                if (events.Items != null && events.Items.Count > 0)
+                {
+                    List<Calendario> dati = new List<Calendario>();
+                    foreach (var eventItem in events.Items)
+                    {
+                        string when = eventItem.Start.DateTime.ToString();
+                        if (String.IsNullOrEmpty(when))
+                        {
+                            when = eventItem.Start.Date;
+                        }
+                        dati.Add(new Calendario { Evento = eventItem.Summary, Data = when });
+                    }
+                    dgvVisualizzaDati.ItemsSource = dati;
+                }
+                else
+                {
+                    MessageBox.Show("No upcoming events found.");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+    }
+}
